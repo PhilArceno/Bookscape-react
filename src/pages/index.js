a74546cb8392bb5100626b6d544cc061bf0e35a1
Dmitri Banshchikov <86620578+dzimazilla@users.noreply.github.com>
2022-03-16 12:11:27 -0400

    added export for Login, Signup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 //NOTE: we will collect files to bundle them up in index files! (usually an index for each folder)
 import Home from "./Home";
 import PageNotFound from "./PageNotFound";
+import Login from "./Login";
+import Signup from "./Signup";
 
-export {Home, PageNotFound}
+export {Home, PageNotFound, Login, Signup}
