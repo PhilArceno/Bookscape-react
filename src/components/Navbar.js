889ef46a6175c150b28709a0320480a5aa983398
Dmitri Banshchikov <86620578+dzimazilla@users.noreply.github.com>
2022-03-16 12:10:48 -0400

    changed default links to login and signup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -82,7 +82,7 @@ export default function Navbar() {
                 fontSize={'sm'}
                 fontWeight={400}
                 variant={'link'}
-                href={'#'}
+                href={'login'}
               >
                 Sign In
               </Button>
@@ -92,7 +92,7 @@ export default function Navbar() {
                 fontWeight={600}
                 color={'white'}
                 bg={'pink.400'}
-                href={'#'}
+                href={'signup'}
                 _hover={{
                   bg: 'pink.300',
                 }}
