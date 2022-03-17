89b19030bcc0354484ce3c6ec96bc2971a90f82f
Dmitri Banshchikov <86620578+dzimazilla@users.noreply.github.com>
2022-03-17 09:32:55 -0400

    deleted LoadingCard

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,6 @@ import Testimonials from "./Testimonials"
 import Navbar from "./Navbar"
 import LoadingBook from "./LoadingBook"
 import SearchAPIBook from "./SearchAPIBook"
-import LoadingCard from "./LoadingCard"
 
-export {Hero, Features, Footer, Testimonials, Navbar, LoadingBook, SearchAPIBook, LoadingCard}
+
+export {Hero, Features, Footer, Testimonials, Navbar, LoadingBook, SearchAPIBook}
