6e706550328ef05e8f197f7cd912b43ff709513d
PhilArceno <54872569+PhilArceno@users.noreply.github.com>
2022-03-22 23:32:43 -0400

    import adjustment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, {useEffect} from 'react';
 import { ChakraProvider, theme } from '@chakra-ui/react';
-import { Home, PageNotFound, Login, Signup, GoogleBooksSearch, Books } from './pages/';
+import * as Pages from './pages/';
 import { Footer, Navbar } from './components';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import { GoogleBooksDetails } from './pages/librarian';
-import {UserAdd,UserDetail,UserList,UserEdit,BookList,BookAdd,BookEdit,BookDetail} from './pages/admin';
+import * as AdminPages from './pages/admin';
 
 
 function App() {
@@ -18,23 +18,22 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" exact element={<Home/>} />
-          <Route path="/home" exact element={<Home/>} />
-          <Route path="/login" exact element={<Login />} />
-          <Route path="/signup" exact element={<Signup />} />
-          <Route path="/books" exact element={<Books />} />
+          <Route path="/" exact element={<Pages.Home/>} />
+          <Route path="/home" exact element={<Pages.Home/>} />
+          <Route path="/login" exact element={<Pages.Login />} />
+          <Route path="/signup" exact element={<Pages.Signup />} />
+          <Route path="/books" exact element={<Pages.Books />} />
           <Route path="/google-books-search" exact element={<GoogleBooksSearch />} />
           <Route path="/google-books/:id" exact element={<GoogleBooksDetails/>} />
-          <Route exact path='/admin/user/list' element={<UserList/>}/>
-          <Route exact path="/admin/user/:id" element={<UserDetail/>}/>
-          <Route exact path="/admin/user/add" element={<UserAdd/>}/>
-          <Route exact path="/admin/user/edit/:id" element={<UserEdit/>}/>
-          <Route exact path='/admin/book/list' element={<BookList/>}/>
-          <Route exact path="/admin/book/:id" element={<BookDetail/>}/>
-          <Route exact path="/admin/book/add" element={<BookAdd/>}/>
-          <Route exact path="/admin/book/edit/:id" element={<BookEdit/>}/>
-
-          <Route path="*" exact element={<PageNotFound />} />
+          <Route exact path='/admin/user/list' element={<AdminPages.UserList/>}/>
+          <Route exact path="/admin/user/:id" element={<AdminPages.UserDetail/>}/>
+          <Route exact path="/admin/user/add" element={<AdminPages.UserAdd/>}/>
+          <Route exact path="/admin/user/edit/:id" element={<AdminPages.UserEdit/>}/>
+          <Route exact path='/admin/book/list' element={<AdminPages.BookList/>}/>
+          <Route exact path="/admin/book/:id" element={<AdminPages.BookDetail/>}/>
+          <Route exact path="/admin/book/add" element={<AdminPages.BookAdd/>}/>
+          <Route exact path="/admin/book/edit/:id" element={<AdminPages.BookEdit/>}/>
+          <Route path="*" exact element={<Pages.PageNotFound />} />
         </Routes>
         <Footer />
       </BrowserRouter>
