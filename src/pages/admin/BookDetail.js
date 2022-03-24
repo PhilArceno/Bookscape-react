1294f1bcf1a95f0e1c845ce397a4b9d2084eb823
lynne <lynnelin2019@gmail.com>
2022-03-24 00:29:02 -0400

    update

---
+++ src/pages/admin/BookDetail.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function BookDetail() {
+
+  
+
+  return (
+    <div className="container">
+    Book Detail
+    </div>
+    );
+  }
+  
+  export default BookDetail;
