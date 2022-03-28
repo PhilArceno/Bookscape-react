f13b634418b1e79ab98b64ce93cdb929effac601
PhilArceno <54872569+PhilArceno@users.noreply.github.com>
2022-03-28 10:02:10 -0400

    a to link

--- src/pages/admin/AdminDashboard.js
+++ src/pages/admin/AdminDashboard.js
@@ -5,8 +5,8 @@ import {Link} from 'react-router-dom';
 function AdminDashboard(){
     return (
         <div className="container">
-           <Link href="/admin/user/list">User List</Link><br/>
-           <Link href="/admin/book/list">Book List</Link><br/>
+           <Link to="/admin/user/list">User List</Link><br/>
+           <Link to="/admin/book/list">Book List</Link><br/>
            {/* <a href="#">Link</a><br/>
            <a href="#">Link</a> */}
         </div>);
