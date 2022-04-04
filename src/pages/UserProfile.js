cc7f212a8edb3fef4a9a32ab38cd0e691201cc67
Dmitri Banshchikov <86620578+dzimazilla@users.noreply.github.com>
2022-04-03 20:54:42 -0400

    updated button

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -24,8 +24,8 @@ export default function UserProfile() {
     navigate("/editprofile");
 };
 
-const UserLoans = ()=>{
-  navigate("/borrowedbooks");
+const ResetPassword = ()=>{
+  navigate("/resetpassword");
 };
 
   useEffect(() => {
@@ -89,15 +89,15 @@ const UserLoans = ()=>{
 
         <Stack spacing={6} direction={['column', 'row']}>
           <Button
-            bg={'green.400'}
+            bg={'red.400'}
             color={'white'}
             w="full"
             _hover={{
-              bg: 'green.500',
+              bg: 'red.500',
             }}
-            onClick={()=>{UserLoans()}}
+            onClick={()=>{ResetPassword()}}
           >
-            My Borrowed Books
+            Reset Password
           </Button>
           <Button
             bg={'blue.400'}
