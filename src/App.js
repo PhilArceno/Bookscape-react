b2531910b5fe210b131e6cd11483f6962023c147
Dmitri Banshchikov <86620578+dzimazilla@users.noreply.github.com>
2022-04-03 20:55:39 -0400

    changed redirection --> books

--- src/App.js
+++ src/App.js
@@ -110,7 +110,7 @@ function App() {
       // trying to go to when they were redirected. This allows us to send them
       // along to that page after they login, which is a nicer user experience
       // than dropping them off on the home page.
-      return <Navigate to="/login" state={{ from: location }} />;
+      return <Navigate to="/books" state={{ from: location }} />;
     }
 
     return <Outlet />;
