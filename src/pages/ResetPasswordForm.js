ff47880e2f0430d031742c65c8e7d069b34c760b
Dmitri Banshchikov <86620578+dzimazilla@users.noreply.github.com>
2022-04-05 12:43:03 -0400

    done

--- src/pages/ResetPasswordForm.js
+++ src/pages/ResetPasswordForm.js
@@ -11,7 +11,7 @@ import {
   InputGroup,
   InputRightElement,
 } from '@chakra-ui/react';
-import { useRef, useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Axios from 'axios';
 import { config } from '../helpers/constants';
@@ -35,50 +35,46 @@ export default function ResetPasswordForm() {
   const [user, setUser] = useState({});
 
   const [showPassword, setShowPassword] = useState(false);
-  const submitBtn = useRef(null);
-  const [formSuccess, setFormSuccess] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
 
   const {
     register,
     handleSubmit,
-    setValue,
-    formState: { errors }
+    formState: { errors },
   } = useForm({
-    resolver: yupResolver(validationSchema)
+    resolver: yupResolver(validationSchema),
   });
 
-  useEffect(()=>{
-    Axios.get(config.url.API_URL+`/api/Users/${authState.id}`,{
-      headers : {
-        'Authorization': `Bearer ${localStorage.getItem("accessToken")}`, 
-      }
+  useEffect(() => {
+    Axios.get(config.url.API_URL + `/api/Users/${authState.id}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+      },
+    }).then(response => {
+      console.log(response.data);
+      var user = response.data;
+      setUser(user);
+    });
+  }, []);
+
+  const onSubmit = data => {
+    console.log(data);
+    fetch(config.url.API_URL + `/api/Users/${authState.id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        Password: data.password,
+        PasswordConfirm: data.confirmPassword,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+      },
     })
-      .then((response)=>{
-        console.log(response.data);
-        var user = response.data;
-        setUser(user);
-      });
-    },[]);
-
-    const onSubmit = (data) => {
-      console.log(data);
-      fetch(config.url.API_URL + `/api/Users/${authState.id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-          password: data.password,
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${localStorage.getItem("accessToken")}`, 
-        },
-      }).then((response) => response.text())
-      .then((text) => {
+      .then(response => response.text())
+      .then(text => {
         console.log(text);
-        navigate("/myprofile");
+        navigate('/myprofile');
       });
-    }
-
+  };
 
   return (
     <Flex
@@ -98,61 +94,61 @@ export default function ResetPasswordForm() {
         my={12}
       >
         <form onSubmit={handleSubmit(onSubmit)}>
-        <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }}>
-          Reset password
-        </Heading>
-        <FormControl id="password" isRequired>
-          <FormLabel>Password</FormLabel>
-          <Text color="red">{errors.password?.message}</Text>
-          <InputGroup>
-            <Input
-              type={showPassword ? 'text' : 'password'}
-              placeholder="Enter a 8-24 length password"
-              {...register('password')}
-            />
-            <InputRightElement h={'full'}>
-              <Button
-                variant={'ghost'}
-                onClick={() => setShowPassword(showPassword => !showPassword)}
-              >
-                {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-              </Button>
-            </InputRightElement>
-          </InputGroup>
-        </FormControl>
-        <FormControl id="confirmPassword" isRequired>
-          <FormLabel>Confirm Password</FormLabel>
-          <Text color="red">{errors.confirmPassword?.message}</Text>
-          <InputGroup>
-            <Input
-              data-testid="confirmPasswordInput"
-              type={showPassword ? 'text' : 'password'}
-              placeholder="Enter a 8-24 length password"
-              {...register('confirmPassword')}
-            />
-            <InputRightElement h={'full'}>
-              <Button
-                variant={'ghost'}
-                onClick={() => setShowPassword(showPassword => !showPassword)}
-              >
-                {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-              </Button>
-            </InputRightElement>
-          </InputGroup>
-        </FormControl>
-        <Stack spacing={6}>
-        <Button
-                  type="submit"
-                  size="lg"
-                  bg={'blue.400'}
-                  color={'white'}
-                  _hover={{
-                    bg: 'blue.500',
-                  }}
+          <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }}>
+            Reset password
+          </Heading>
+          <FormControl id="password" isRequired>
+            <FormLabel>Password</FormLabel>
+            <Text color="red">{errors.password?.message}</Text>
+            <InputGroup>
+              <Input
+                type={showPassword ? 'text' : 'password'}
+                placeholder="Enter a 8-24 length password"
+                {...register('password')}
+              />
+              <InputRightElement h={'full'}>
+                <Button
+                  variant={'ghost'}
+                  onClick={() => setShowPassword(showPassword => !showPassword)}
                 >
-                  Submit
+                  {showPassword ? <ViewIcon /> : <ViewOffIcon />}
                 </Button>
-        </Stack>
+              </InputRightElement>
+            </InputGroup>
+          </FormControl>
+          <FormControl id="confirmPassword" isRequired>
+            <FormLabel>Confirm Password</FormLabel>
+            <Text color="red">{errors.confirmPassword?.message}</Text>
+            <InputGroup>
+              <Input
+                data-testid="confirmPasswordInput"
+                type={showPassword ? 'text' : 'password'}
+                placeholder="Enter a 8-24 length password"
+                {...register('confirmPassword')}
+              />
+              <InputRightElement h={'full'}>
+                <Button
+                  variant={'ghost'}
+                  onClick={() => setShowPassword(showPassword => !showPassword)}
+                >
+                  {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+          </FormControl>
+          <Stack spacing={6}>
+            <Button
+              type="submit"
+              size="lg"
+              bg={'blue.400'}
+              color={'white'}
+              _hover={{
+                bg: 'blue.500',
+              }}
+            >
+              Submit
+            </Button>
+          </Stack>
         </form>
       </Stack>
     </Flex>
