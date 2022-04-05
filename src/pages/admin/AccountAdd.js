81eb412839ecac64f4492f387241a3b631aac035
lynne <lynnelin2019@gmail.com>
2022-04-05 17:37:33 -0400

    update

--- src/pages/admin/AccountAdd.js
+++ src/pages/admin/AccountAdd.js
@@ -1,15 +1,108 @@
-import Reac from 'react';
+import React, { useState,useEffect } from 'react';
+import { useNavigate,useParams,Link } from "react-router-dom";
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+import Axios from 'axios';
+import { config } from '../../helpers/constants';
 
 
 function AccountAdd() {
 
-  
+  let {id} = useParams();
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+  const [account, setAccount] = useState({});
+
+
+  const onSubmit = data => {
+    
+    //Axios.post(config.url.API_URL+'/api/AccountOperations/payment/'
+    /*Axios.post('https://localhost:7098/api/AccountOperations/payment/'
+    
+      +data.userId+'?amount='+data.amount,
+    { 
+     // amount:20,
+      //Amount:data.userId,
+      
+    },
+      {
+      headers : {
+        'Authorization': `Bearer ${localStorage.getItem("accessToken")}`, 
+      }
+    })*/
+    Axios({
+      method: 'post',
+      url: config.url.API_URL+'/api/AccountOperations/payment/'+data.userId
+        +'?amount='+data.amount,
+      data: {
+       
+      },
+      headers: {'Authorization': `Bearer ${localStorage.getItem("accessToken")}`, }
+    })
+      .then((response)=>{
+          navigate("/admin/account/list");
+      }).catch((error)=>{
+        setError(error.message);
+      });
+  };
+  const validationSchema = Yup.object().shape({
+    amount: Yup.string()
+    .required(' Amount is required'),
+     
+     
+    });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(validationSchema)
+  });
 
 
   return (
     <div className="container">
-        AccountAdd
-   </div>
+    {error?(<div className="alert alert-danger">{error}</div>):""}
+    <div className="row">
+    <div className ="col-lg-6 col-md-6 col-sm-6 container justify-content-center card"><br/>
+        <h3 className = "text-center"> Create New Payment </h3>
+				<div className = "card-body"></div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+    <input name="Id" type="hidden" value={account.userId}/>
+    <div className="form-group">
+        <label>User ID</label>
+        <input
+          name="userId"
+          type="text"
+          {...register('userId')}
+          className={`form-control ${errors.userId ? 'is-invalid' : ''}`}
+        />
+        <div className="invalid-feedback">{errors.userId?.message}</div>
+      </div> 
+      
+       
+      <div className="form-group">
+        <label>Amount</label>
+        <input
+          name="amount"
+          type="decimal"
+          {...register('amount')}
+          className={`form-control ${errors.amount ? 'is-invalid' : ''}`}
+        />
+        <div className="invalid-feedback">{errors.amount?.message}</div>
+      </div> 
+     
+       
+      <div className="form-group" ><br/>
+        <button type="submit" className="btn btn-primary m-2"  >
+          Submit
+        </button>
+        <Link to="/admin/account/list" className="btn btn-danger ml-2">Cancel</Link>
+      </div><br/>
+    </form>
+    </div></div></div>
     );
   }
   
--- src/pages/admin/AccountOperationList.js
+++ src/pages/admin/AccountOperationList.js
@@ -22,18 +22,15 @@ function AccountOperationList() {
       data.forEach(d => {
         var account = {};
         account.id = d.id;
-        account.operationType = d.operationType.operationType;
-        account.startDate= d.loan.startDate;
-        account.dueDate = d.loan.dueDate;
-        account.returnDate= d.loan.returnDate;
+        account.operationType = d.operationType;
+        account.amount = d.amount;
+        account.recordedTime = d.recordedTime;
+
+        
         account.userId = d.user.id;
         account.userName = d.user.userName;
-        account.bookId = d.book.id;
-        account.title = d.book.title;
-        account.isbn = d. book.isbn;
-        account.coverImage = d.book.coverImage
-        account.amount = d.amount
-        account.recordeTime = d.recordeTime;
+
+       
         accountList.push(account);
       })
       setAccountList(accountList);
@@ -45,6 +42,10 @@ function AccountOperationList() {
 
   const editAccount = (id)=>{
       navigate("/admin/account/edit/"+id);
+    };
+    
+    const balanceAccount = (id)=>{
+      navigate("/admin/account/balance/"+id);
     };
    
   const deleteAccount = (id)=>{
@@ -73,33 +74,24 @@ function AccountOperationList() {
          <table class = "table table-striped table-bordered">
            <thead class = "table-dark">
         <tr>
-       
-      
-        <th>Book ISBN</th>
         <th>UserName</th>
-        <th>StartDate</th>
-        <th>DueDate</th>
-        <th>returnDate</th>
         <th>Amount</th>
-        <th>Recorder Time</th>
-        
+        <th>Recorderd Time</th>
+
         <th>Update</th>
         <th>Delete</th>
-
+       
+         
         </tr>
            </thead>
            <tbody>
         {accountList.length > 0 ? accountList.map((account)=>{
           return (<tr key={(account.id)}>
-          
-            <td><a href={'/admin/account/'+account.id}>{account.isbn}</a></td>
-            <td>{account.userName}</td>
-            <td>{account.startDate}</td>
-            <td>{account.dueDate}</td> 
-            <td>{account.returnDate}</td>
+            <td><a href={'/admin/account/'+account.id}>{account.userName}</a></td>
             <th>{account.amount}</th>   
-            <th>{account.recordeTime }</th> 
-             
+            <th>{account.recordedTime }</th> 
+           
+          
             <td><button class = "btn btn-primary" onClick={()=>{editAccount(account.id)}}>Update</button></td>
             <td><button class = "btn btn-danger" onClick={()=>{deleteAccount(account.id)}}>Delete</button></td>    
           </tr>)
