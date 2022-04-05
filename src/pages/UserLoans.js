fc2bb27a5da11a649301397b2f34eb71bbe62d04
Dmitri Banshchikov <86620578+dzimazilla@users.noreply.github.com>
2022-04-04 21:40:22 -0400

    updated

--- src/pages/UserLoans.js
+++ src/pages/UserLoans.js
@@ -1,93 +1,102 @@
-import { Box, Button, Heading} from '@chakra-ui/react';
-import {
-  Table,
-  Thead,
-  Tbody,
-  Tfoot,
-  Tr,
-  Th,
-  Td,
-  TableCaption,
-} from '@chakra-ui/react';
-import React, { useState, useEffect, useContext } from 'react';
+import { Box, Button, Heading } from '@chakra-ui/react';
+import { Table, Thead, Tbody, Tr, Th, Td, Image, Text } from '@chakra-ui/react';
+import React, { useState, useEffect, useContext, useRef } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import Axios from 'axios';
 import { config } from '../helpers/constants';
 import { AuthContext } from '../helpers/contexts/';
 
 function UserLoans() {
-  const [error, setError] = useState('');
+  const [message, setMessage] = useState('');
   const [loanList, setLoanList] = useState([]);
   const navigate = useNavigate();
   const { authState } = useContext(AuthContext);
   const [renewStatus, setRenewStatus] = useState(false);
-
+  const submitBtn = useRef(null);
 
   useEffect(() => {
-    Axios.get(config.url.API_URL + "/api/loans/active",{
-      headers : {
-        'Authorization': `Bearer ${localStorage.getItem("accessToken")}`, 
-      }
+    Axios.get(config.url.API_URL + '/api/loans/active', {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+      },
     }).then(response => {
       console.log(response);
       setLoanList(response.data);
     });
   }, []);
 
-  const renewLoan = (id) => {
-    Axios.put(config.url.API_URL+`/api/loans/renew/${id}`, {
+  const renewLoan = id => {
+    fetch(config.url.API_URL + `/api/loans/renew/${id}`, {
+      method: 'PUT',
       headers: {
-        'Authorization': `Bearer ${localStorage.getItem("accessToken")}`, 
-      }
-  }).then(response => response.text())
-  .then(text => {
-    if (text) setRenewStatus(true);
-    console.log (text);
-  })};
-
+        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+      },
+    })
+      .then(async response => {
+        if (!response.ok) {
+          throw Error(await response.text());
+        } else return response.text();
+      })
+      .then(text => {
+        setMessage('Loan has been successfully renewed');
+      })
+      .catch(err => {
+        setMessage(err.message);
+      });
+  };
 
   return (
-    <><Box m={10} maxW="1600" display="flex" justifyContent={'center'}>
-    <Heading text-align={'center'}>My borrowed books</Heading>
-    </Box>
-    <Box m={10} maxW="1600" display="flex" justifyContent={'center'}>
-
-      <Table>
-        <Thead>
-          {' '}
-          <Tr>
-          <Th>Title</Th>
-          <Th>Author</Th>
-            <Th>Start Day</Th>
-            <Th>Due Day</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {loanList.length > 0
-            ? loanList.map(loan => {
-                return (
-                  <Tr key={loan.id}>
-                    <Td>{loan.book.title}</Td>
-                    <Td>{loan.book.author}</Td>
-                    <Td>{loan.startDate.substring(0, 10)}</Td>
-                    <Td>{loan.dueDate.substring(0, 10)}</Td>
-
-                    <Button
-                      colorScheme="teal"
-                      size="md"
-                      onClick={() => {
-                        renewLoan(loan.id);
-                      }}
-                    >
-                      Renew book
-                    </Button>
-                  </Tr>
-                );
-              })
-            : ''}
-        </Tbody>
-      </Table>
-    </Box>
+    <>
+      <Box m={10} maxW="1600" display="flex" justifyContent={'center'}>
+        <Heading text-align={'center'}>My borrowed books</Heading>
+      </Box>
+      <Box m={10} maxW="1600" display="flex" justifyContent={'center'}>
+        <Text fontSize='4xl' color='tomato' as='mark'>{message}</Text>
+      </Box>
+      <Box m={10} maxW="1600" display="flex" justifyContent={'center'}>
+        <Table>
+          <Thead>
+            {' '}
+            <Tr>
+              <Th>Cover Image</Th>
+              <Th>Title</Th>
+              <Th>Author</Th>
+              <Th>Start Day</Th>
+              <Th>Due Day</Th>
+              <Th>Renew</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {loanList.length > 0
+              ? loanList.map(loan => {
+                  return (
+                    <Tr key={loan.id}>
+                      <Td>
+                        <Image src={loan.book.coverImage} maxH="20" />
+                      </Td>
+                      <Td>{loan.book.title}</Td>
+                      <Td>{loan.book.author}</Td>
+                      <Td>{loan.startDate.substring(0, 10)}</Td>
+                      <Td>{loan.dueDate.substring(0, 10)}</Td>
+                      <Td>
+                        <Button
+                          ref={submitBtn}
+                          colorScheme="teal"
+                          size="md"
+                          onClick={() => {
+                            renewLoan(loan.id);
+                          }}
+                        >
+                          Renew book
+                        </Button>
+                      </Td>
+                    </Tr>
+                  );
+                })
+              : ''}
+          </Tbody>
+        </Table>
+      </Box>
     </>
   );
 }
