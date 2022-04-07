bf6bdeba059c731203d60c982eda0f0a44b24c3a
PhilArceno <54872569+PhilArceno@users.noreply.github.com>
2022-04-07 16:08:36 -0400

    stripe started

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,4 +1,5 @@
 import GoogleBooksDetails from "../user/GoogleBooksDetails";
 import GoogleBooksSearch from "./GoogleBooksSearch";
+import MyBalance from "./MyBalance";
 
-export { GoogleBooksDetails, GoogleBooksSearch }
+export { GoogleBooksDetails, GoogleBooksSearch, MyBalance }
